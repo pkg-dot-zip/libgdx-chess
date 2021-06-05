1144e35554497bad3f70b4d7ca256420c6cc7fff
medkam <med.dekam@student.avans.nl>
2021-06-05 21:11:32 +0200

    Implemented detection for broken connections
    Both clients now send constant pings to eachother, if one stops sending for whatever reason or completely disconnects then the server makes the other client win.
    If the clients can't connect to the server then it's a draw.

--- core/src/com/zimonishim/chess/util/networking/Client.java
+++ core/src/com/zimonishim/chess/util/networking/Client.java
@@ -17,42 +17,68 @@ public class Client {
     public static final String IP = "localhost";
     public static final int PORT = 8000;
 
+    private Socket socket;
     private ObjectOutputStream objectOutputStream;
+
+    private Thread pingServer = new Thread(new PingServer());
+    private Thread timeoutCheck = new Thread(new TimeoutCheck());
 
     private Players player;
 
     public Client(IChessBoardCallback chessBoardCallback) {
-        new Thread(() -> {
+        try {
+            socket = new Socket(IP, PORT);
+            System.out.println("Initialised socket.");
 
-            try {
-                Socket socket = new Socket(IP, PORT);
-                System.out.println("Initialised socket.");
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            System.out.println("Initialised objectInputStream.");
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            System.out.println("Initialised objectOutputStream.");
 
-                ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-                System.out.println("Initialised objectInputStream.");
-                objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
-                System.out.println("Initialised objectOutputStream.");
+            player = (objectInputStream.readInt() == 0) ? Players.WHITE : Players.BLACK;
 
-                player = (objectInputStream.readInt() == 0) ? Players.WHITE : Players.BLACK;
 
+
+            new Thread(() -> {
                 while (true) {
                     try {
                         List<ChessField> chessFieldArrayList = (ArrayList<ChessField>) objectInputStream.readObject();
-                        System.out.println("Received new board state: " + chessFieldArrayList);
+                        lastPingReceived = System.currentTimeMillis();
+                        // Null list is a client connection ping.
+                        if (chessFieldArrayList != null) {
+                            System.out.println("Received new board state: " + chessFieldArrayList);
 
-                        chessBoardCallback.setChessFields(chessFieldArrayList);
-                        chessBoardCallback.switchTurn();
+                            // The list being empty means that the server lost connection with the other client and this client should win.
+                            if (chessFieldArrayList.size() == 0) {
+                                // TODO This client should win now.
+                                System.out.println("Other client disconnected. YOU WON!");
+                                stopConnectionCheck();
+                                break;
 
-                    } catch (ClassNotFoundException classNotFoundException) {
-                        classNotFoundException.printStackTrace();
+                            } else {
+                                chessBoardCallback.setChessFields(chessFieldArrayList);
+                                chessBoardCallback.switchTurn();
+                            }
+                        } else {
+                            System.out.println("Received a ping");
+                        }
+
+                    } catch (ClassNotFoundException | IOException exception) {
+                        exception.printStackTrace();
+
+                        // If a problem occured in receiving the data then call a draw and stop trying to read new data.
+                        System.out.println("try catch resulting in draw");
+                        callDraw();
+                        break;
                     }
                 }
+            }).start();
 
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }).start();
+            pingServer.start();
+            timeoutCheck.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendChessFields(IChessBoardCallback chessBoardCallback) {
@@ -63,6 +89,73 @@ public class Client {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private int pingTimeMs = 5000;
+
+    /**
+     * Keep on sending a ping of a blank chessfield so the server and other client know that there's a connection.
+     */
+    private class PingServer implements Runnable {
+        @Override
+        public void run() {
+            long systemTime = System.currentTimeMillis();
+            while (true) {
+                // A ping every 5 seconds.
+                if (System.currentTimeMillis() - systemTime >= pingTimeMs) {
+                    systemTime = System.currentTimeMillis();
+                    System.out.println("Sending a ping");
+                    try {
+                        // Null object is the ping.
+                        objectOutputStream.writeObject(null);
+                        objectOutputStream.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private long lastPingReceived = System.currentTimeMillis();
+
+    /**
+     * Constantly check if the timeout time has been crossed.
+     */
+    private class TimeoutCheck implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                if (System.currentTimeMillis() - lastPingReceived > 3 * pingTimeMs) {
+                    // Server connection broken, so call a draw.
+                    System.out.println("Timeout resulting in draw");
+                    callDraw();
+                }
+
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void stopConnectionCheck() {
+        pingServer.stop();
+        timeoutCheck.stop();
+    }
+
+    //TODO Call a draw for after the connection was broken in any way.
+    public void callDraw() {
+        // Close the socket, networking is no longer needed.
+        System.out.println("Lost connection with the server. IT'S A DRAW!");
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stopConnectionCheck();
     }
 
     public Players getPlayer() {
--- core/src/com/zimonishim/chess/util/networking/Server.java
+++ core/src/com/zimonishim/chess/util/networking/Server.java
@@ -1,66 +1,122 @@
 package com.zimonishim.chess.util.networking;
 
+import com.zimonishim.chess.gameObjects.ChessField;
+
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 
 public class Server {
+    private static ObjectOutputStream objectOutputStreamSocketOne;
+    private static ObjectOutputStream objectOutputStreamSocketTwo;
 
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(Client.PORT);
 
+            Socket socket1 = serverSocket.accept();
+            System.out.println("Accepted socket1.");
+            Socket socket2 = serverSocket.accept();
+            System.out.println("Accepted socket2.");
+
+            objectOutputStreamSocketOne = new ObjectOutputStream(socket1.getOutputStream());
+            ObjectInputStream objectInputStream1 = new ObjectInputStream(socket1.getInputStream());
+            objectOutputStreamSocketTwo = new ObjectOutputStream(socket2.getOutputStream());
+            ObjectInputStream objectInputStream2 = new ObjectInputStream(socket2.getInputStream());
+            System.out.println("Initialised input and output streams.");
+
+            objectOutputStreamSocketOne.writeInt(0);
+            objectOutputStreamSocketOne.flush();
+            objectOutputStreamSocketTwo.writeInt(1);
+            objectOutputStreamSocketTwo.flush();
+
             new Thread(() -> {
+                while (true) {
+                    ArrayList<ChessField> chessFields;
+                    // Read from socket 2.
+                    try {
+                        chessFields = (ArrayList<ChessField>) objectInputStream2.readObject();
 
-                try {
-                    Socket socket1 = serverSocket.accept();
-                    System.out.println("Accepted socket1.");
-                    Socket socket2 = serverSocket.accept();
-                    System.out.println("Accepted socket2.");
+                        System.out.println("Read input from socket 2");
+                    } catch (ClassNotFoundException | IOException classNotFoundException) {
+                        // One of the sockets has been closed.
+                        System.out.println("Connection with socket 2 broken");
+                        connectionBrokenWithSocketTwo();
+                        break;
+                    }
 
-                    ObjectOutputStream objectOutputStream1 = new ObjectOutputStream(socket1.getOutputStream());
-                    ObjectInputStream objectInputStream1 = new ObjectInputStream(socket1.getInputStream());
-                    ObjectOutputStream objectOutputStream2 = new ObjectOutputStream(socket2.getOutputStream());
-                    ObjectInputStream objectInputStream2 = new ObjectInputStream(socket2.getInputStream());
-                    System.out.println("Initialised input and output streams.");
-
-                    objectOutputStream1.writeInt(0);
-                    objectOutputStream1.flush();
-                    objectOutputStream2.writeInt(1);
-                    objectOutputStream2.flush();
-
-                    new Thread(() -> {
-                        while (true) {
-                            try {
-                                objectOutputStream1.writeObject(objectInputStream2.readObject());
-                                System.out.println("Sent object from socket 2 to 1.");
-                            } catch (ClassNotFoundException | IOException classNotFoundException) {
-                                classNotFoundException.printStackTrace();
-                            }
-                        }
-                    }).start();
-
-                    new Thread(() -> {
-                        while (true) {
-                            try {
-                                objectOutputStream2.writeObject(objectInputStream1.readObject());
-                                System.out.println("Sent object from socket 1 to 2.");
-                            } catch (ClassNotFoundException | IOException classNotFoundException) {
-                                classNotFoundException.printStackTrace();
-                            }
-                        }
-                    }).start();
-
-                } catch (IOException e) {
-                    e.printStackTrace();
+                    // Write to socket one.
+                    try {
+                        objectOutputStreamSocketOne.writeObject(chessFields);
+                        System.out.println("Sent output to socket 1");
+                    } catch (IOException e) {
+                        // One of the sockets has been closed.
+                        System.out.println("Connection with socket 1 broken");
+                        connectionBrokenWithSocketOne();
+                        break;
+                    }
                 }
-
             }).start();
+
+            new Thread(() -> {
+                while (true) {
+                    ArrayList<ChessField> chessFields;
+
+                    // Read from socket 1.
+                    try {
+                        chessFields = (ArrayList<ChessField>) objectInputStream1.readObject();
+
+                        System.out.println("Read input from socket 1");
+                    } catch (ClassNotFoundException | IOException classNotFoundException) {
+                        // One of the sockets has been closed
+                        System.out.println("Connection with socket 1 broken");
+                        connectionBrokenWithSocketOne();
+                        break;
+                    }
+
+                    // Write to socket 2.
+                    try {
+                        objectOutputStreamSocketTwo.writeObject(chessFields);
+                        System.out.println("Sent output to socket 2");
+                    } catch (IOException e) {
+                        // One of the sockets has been closed.
+                        System.out.println("Connection with socket 2 broken");
+                        connectionBrokenWithSocketTwo();
+                        break;
+                    }
+                }
+            }).start();
+
 
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static synchronized void connectionBrokenWithSocketOne() {
+        // Socket one lost connection, so make socket two win.
+        try {
+            System.out.println("Trying to make socket 1 win");
+            objectOutputStreamSocketTwo.writeObject(new ArrayList<ChessField>());
+            objectOutputStreamSocketTwo.flush();
+            System.out.println("Succesfully sent message to socket two to win");
+        } catch (IOException e) {
+            System.out.println("Failed in sending message to socket two to win");
+        }
+    }
+
+    public static synchronized void connectionBrokenWithSocketTwo() {
+        // Socket two lost connection, so make socket one win.
+        try {
+            System.out.println("Trying to make socket 2 win");
+            objectOutputStreamSocketOne.writeObject(new ArrayList<ChessField>());
+            objectOutputStreamSocketOne.flush();
+            System.out.println("Succesfully sent message to socket one to win");
+        } catch (IOException e) {
+            System.out.println("Failed in sending message to socket one to win");
         }
     }
 }
